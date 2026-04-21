eaabc95af8f622b5170cbc30af179909176dca8c
Andreas Bräuer <braeuer.andreas.ba@gmail.com>
2026-04-21 21:38:14 +0200

    added a debug flag, which adds optional  debug informaiton to the output

--- andreas/simulation_algorithm.py
+++ andreas/simulation_algorithm.py
@@ -87,8 +87,9 @@ def case_g_h_Vout_helper(params, Vint):
 
 
 # this code could be improved, basically represents algorithm1 from paper, but could use some optimization trough refactoring
-def algorithm1(input_transitions: list[InputTransition], params: NORModelParams) -> list[OutputTransition]:
+def algorithm1(input_transitions: list[InputTransition], params: NORModelParams, debug=False):
     O: list[OutputTransition] = []
+    debug_infos: list[dict] = []
 
     # Line 1: initializing variables
     T = float('inf')                # ∞
@@ -125,7 +126,8 @@ def algorithm1(input_transitions: list[InputTransition], params: NORModelParams)
         case = determine_case(current_transition.x, current_transition.y)
 
         if case == Case.A or case == Case.B or case == Case.C or case == Case.D:
-            t_o = current_transition.t + case.delay_func(Vint, params) # a, b, c, d requires these 2 arguments, this could be solved more estetically i am aware, but is pragmatic
+            delay = case.delay_func(Vint, params) # a, b, c, d requires these 2 arguments, this could be solved more estetically i am aware, but is pragmatic
+            t_o = current_transition.t + delay
 
             # Line 13 + Line 15 on
             # Cancellation check, so I dont have to add first and them remove again.
@@ -142,7 +144,8 @@ def algorithm1(input_transitions: list[InputTransition], params: NORModelParams)
             elif case == Case.F:
                 delta_f_temp = current_transition.t
 
-            t_o = current_transition.t + case.delay_func(Vint, params) # e, f requires these 2 arguments, this could be solved more estetically i am aware, but is pragmatic
+            delay = case.delay_func(Vint, params) # e, f requires these 2 arguments, this could be solved more estetically i am aware, but is pragmatic
+            t_o = current_transition.t + delay
 
             # Line 13 + Line 15 on
             # Cancellation check, so I dont have to add first and them remove again.
@@ -154,7 +157,10 @@ def algorithm1(input_transitions: list[InputTransition], params: NORModelParams)
             Vint = case.Vout_func(T + delta_min, params)
 
         elif case == Case.G:
-            delta = current_transition.t - delta_e_temp
+            if delta_e_temp == float('-inf'):
+                delta = 1e6     # saturated: T₁ has always been open, This is necessary, because otherwise delay formula helpers chalculations (chi) would crash, this only executes if case G/H is the first case
+            else:
+                delta = current_transition.t - delta_e_temp
             delta_f_temp = current_transition.t # for computing delta in case h
 
             delay = case.delay_func(delta, Vint, params)  # g requires these 3 arguments, this could be solved more estetically i am aware, but is pragmatic
@@ -173,7 +179,10 @@ def algorithm1(input_transitions: list[InputTransition], params: NORModelParams)
             else:
                 Vint = case.Vout_func(T - case_g_h_Vout_helper(params, Vint) + delta_min, delta, Vint, params, delay)
         elif case == Case.H:
-            delta = delta_f_temp - current_transition.t
+            if delta_f_temp == float('-inf'):
+                delta = -1e6    # saturated: T₁ has always been open, This is necessary, because otherwise delay formula helpers chalculations (chi) would crash, this only executes if case G/H is the first case
+            else:
+                delta = delta_f_temp - current_transition.t
             delta_e_temp = current_transition.t  # for computing delta in case g
 
             delay = case.delay_func(delta, Vint, params)  # g requires these 3 arguments, this could be solved more estetically i am aware, but is pragmatic
@@ -192,8 +201,20 @@ def algorithm1(input_transitions: list[InputTransition], params: NORModelParams)
             else:
                 Vint = case.Vout_func(T - case_g_h_Vout_helper(params, Vint) + delta_min, delta, Vint, params, delay)
 
+        if debug:
+            debug_infos.append({
+                "case": case.label,
+                "t_o": t_o,
+                "delay": delay,
+                "Vint": Vint,
+                "cancelled": is_cancelled,
+                "input_t": current_transition.t,
+            })
+
         index_input += 1
 
+    if debug:
+        return O, debug_infos
     return O
 
 
@@ -209,8 +230,6 @@ def algorithm1(input_transitions: list[InputTransition], params: NORModelParams)
         
 """
 
-def basic_sanity_check():
-    return 0
 
 
 if __name__ == "__main__":
